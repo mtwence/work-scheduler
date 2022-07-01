4ac5b8c7930ffbe4b6ca207370287bb2c47e8ae2
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-01 09:20:58 -0400

    add locale storage save on click for 9 AM row

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,23 @@ var momentDate = moment().format("dddd, MMMM Do");
 var time = $("#currentDay");
 time.text("Current Date: " + momentDate)
 
-var hourText = [
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-    {Text: ""},
-]
+// var hourText = [
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+//     {Text: ""},
+// ]
 
-var saveBtn = $(".saveBtn")
+var saveBtn1 = $("#b1")
+var text1 = $("#t1")
 
-saveBtn.on("click", function() {
-
+saveBtn1.on("click", function() {
+var note1 = text1.val();
+localStorage.setItem("9", note1);
 } )
+
