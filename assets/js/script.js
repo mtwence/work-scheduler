8e2ea7f93dd2a889cd7a6382a350846cc0b13f4f
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-01 09:32:52 -0400

    add function for locally stored text to persist on page refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,13 @@ time.text("Current Date: " + momentDate)
 var saveBtn1 = $("#b1")
 var text1 = $("#t1")
 
-saveBtn1.on("click", function() {
-var note1 = text1.val();
-localStorage.setItem("9", note1);
-} )
+saveBtn1.on("click", function () {
+    var note1 = text1.val();
+    localStorage.setItem("9", note1);
+})
 
+function retain() {
+    var maintain = localStorage.getItem("9");
+    text1.text(maintain)
+}
+retain()
