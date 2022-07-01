9e1f26c9dbe1a8345970c6666dae5b68e51b0891
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-06-30 23:22:06 -0400

    add initial starter code

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+var momentDate = moment().format("dddd, MMMM Do");
+var time = $("#currentDay");
+time.text("Current Date: " + momentDate)
+
+var hourText = [
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+]
+var saveBtn = $(".saveBtn")
+
+saveBtn.on("click", function() {
+
+} )
