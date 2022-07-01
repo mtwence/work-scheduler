8bf0f4abffe5045b43c37193bebbe2e5a8740568
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-01 09:54:41 -0400

    add tags to rest of rows text areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+// Create variable of time formatted using moment 
 var momentDate = moment().format("dddd, MMMM Do");
+// jquery selector on <p> where time is being inserted
 var time = $("#currentDay");
+// Set text in </p> to the moment format 
 time.text("Current Date: " + momentDate)
 
 // var hourText = [
@@ -14,16 +17,25 @@ time.text("Current Date: " + momentDate)
 //     {Text: ""},
 // ]
 
+// THEORY CRAFT VARIABLES AND FUNCTIONS FOR 9AM ROW 
+
+// jquery selector on 9am rows button and text area 
 var saveBtn1 = $("#b1")
 var text1 = $("#t1")
 
+// jquery event listener on click of save button saves string in textarea for 9am row 
 saveBtn1.on("click", function () {
     var note1 = text1.val();
     localStorage.setItem("9", note1);
 })
-
-function retain() {
+// function for locally stored 9am text to persist on page 
+function retain1() {
     var maintain = localStorage.getItem("9");
     text1.text(maintain)
 }
-retain()
+retain1()
+
+// RECREATE CODE FOR REST OF ROWS
+
+// 10AM 
+
