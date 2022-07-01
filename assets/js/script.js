2bafa7b4b24c448e8ae7fc606e2e3019c8177134
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-01 01:18:41 -0400

    add array of objects to store text strings for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 var momentDate = moment().format("dddd, MMMM Do");
 var time = $("#currentDay");
 time.text("Current Date: " + momentDate)
+
+var hourText = [
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+    {Text: ""},
+]
